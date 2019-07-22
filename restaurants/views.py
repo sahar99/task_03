4e2bb53b1ae03f5cabdf7ab59cb99b4a4dd2c4c7
sahar99 <sahar.s_2011@hotmail.com>
2019-07-22 21:06:46 +0300

    done task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,13 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+       "my_list": [
+        {"restaurant_name":"A" ,"food_type":"vegan"},
+        {"restaurant_name":"B", "food_type":"seaFood"},
+        {"restaurant_name":"C", "food_type":"burgers"},
+
+
+        ]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +21,8 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object":
+        {"restaurant_name":"A" ,"food_type":"vegan"},
+       
     }
     return render(request, 'detail.html', context)
